7dfb60c91de757259dfacc8d8ce1fdcfb70e8c50
weiningwei <1479796521@qq.com>
2024-10-30 12:00:43 +0800

    测试注释

--- main.cpp
+++ main.cpp
@@ -1,4 +1,22 @@
+/**
+ * @file main.cpp
+ * @author weiningwei (weiningwei09@qq.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-10-28
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
 #include "easy_logger.h"
+
+
+void test_funtion(int a, int b)
+{
+
+}
+
 
 int main()
 {
